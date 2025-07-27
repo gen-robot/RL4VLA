9a94f63eaf9df9e669b4b3a0951ae7a981c27308
matthewmzy <1829142919@qq.com>
2025-07-27 08:50:33 +0000

    add oft

---
+++ openvla_oft/merge_lora.sh
@@ -0,0 +1,3 @@
+python openvla_oft/vla-scripts/merge_lora_weights_and_save.py \
+        --base_checkpoint Haozhan72/Openvla-oft-SFT-libero-spatial-traj1 \
+        --lora_finetuned_checkpoint_dir openvla_oft/checkpoints/warmup/Openvla-oft-SFT-libero-spatial-traj1+warmup+b8+lr-0.0005+lora-r32+dropout-0.0--image_aug--2000_chkpt
